d6ff09d565943ac35d3b679be843b0d4a3b0f561
Zakharii-Husar <zakhariihusar@gmail.com>
2022-05-27 21:45:53 +0300

    remastered js

--- Script1.js
+++ Script1.js
@@ -1,139 +1,126 @@
 document.addEventListener('DOMContentLoaded', () => {
-let barrel = function () {
-    let user = 0;
-    let lives = 3;
-    let score = 0;
-    let level = 1;
-    let mainSpeed = 3000;
-    let defaultSpeed = 2500;
-    let hitSpeed = 2200;
+    const theGame = () => {
+        const properties = {
+            userInput: 0,
+            computerInput: 1,
+            lives: 3,
+            score: 0,
+            level: 1,
+            gameSpeed: 3000,
+            resetSpeed: 2200,
+            clearIntervalStamp: null
+        }
 
-    // GENERATES RANDOM NUMBER EVERY NTH SECON
-    const generator = setInterval(function () {
-        // SETS DEFAULT HOLES AFTER RESTART AND DEFAULT SELECTOR COLLOR
-        for (i = 1; i < 10; i++) {
-            document.getElementById(i).setAttribute("src", "img/default.gif");
-            document.getElementById(i).style.border = "3px solid blue";
+        const displayState = () => {
+            const scoreDisplay = document.getElementById("score");
+            const levelDisplay = document.getElementById("level");
+            const heart = document.getElementById("hearts");
+
+            scoreDisplay.textContent = "SCORE: " + properties.score;
+            levelDisplay.textContent = `LEVEL: ${properties.level}/3`;
+            scoreDisplay.style.background = "white";
+            levelDisplay.style.background = "white";
+            scoreDisplay.style.color = "red";
+            levelDisplay.style.color = "red";
+            heart.setAttribute("src", `img/${properties.lives}.jpg`)
+        }
+
+        const resetGrid = () => {
+            for (i = 1; i < 10; i++) {
+                document.getElementById(i).setAttribute("src", "img/default.gif");
+                document.getElementById(i).style.border = "3px solid blue";
+            };
+            properties.userInput = 0;
+            document.getElementById(properties.computerInput).setAttribute("src", "img/default.gif")
         };
 
-        //BUTTONS
-        const but1 = document.getElementById("1").addEventListener("click", (function () { user = 1; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but2 = document.getElementById("2").addEventListener("click", (function () { user = 2; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but3 = document.getElementById("3").addEventListener("click", (function () { user = 3; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but4 = document.getElementById("4").addEventListener("click", (function () { user = 4; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but5 = document.getElementById("5").addEventListener("click", (function () { user = 5; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but6 = document.getElementById("6").addEventListener("click", (function () { user = 6; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but7 = document.getElementById("7").addEventListener("click", (function () { user = 7; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but8 = document.getElementById("8").addEventListener("click", (function () { user = 8; document.getElementById(user).style.border = "3px solid yellow"; }));
-        const but9 = document.getElementById("9").addEventListener("click", (function () { user = 9; document.getElementById(user).style.border = "3px solid yellow"; }));
-
-
-        //LIVESE SCORE LEVEL
-        let points = document.getElementById("score");
-        let stage = document.getElementById("level");
-        points.textContent = "SCORE: " + score;
-        stage.textContent = `LEVEL: ${level}/3`;
-        points.style.background = "white";
-        stage.style.background = "white";
-        points.style.color = "red";
-        stage.style.color = "red";
-
-        let heart = document.getElementById("hearts");
-        if (lives === lives) {
-            heart.setAttribute("src", `img/${lives}.jpg`)
+        const listenToUserInput = () => {
+            for (let i = 1; i < 10; i++) {
+                document.getElementById(i).addEventListener("click", (function () {
+                    properties.userInput = i;
+                    document.getElementById(properties.userInput).style.border = "3px solid red";
+                }));
+            }
         };
 
-        const num = (Math.floor((Math.random() * 9) + 1));
-        let holes = document.getElementById(num);
-        holes.setAttribute("src", "img/slow.gif");
+        const generateComputerInput = () => {
+            properties.computerInput = (Math.floor((Math.random() * 9) + 1));
+            document.getElementById(properties.computerInput).setAttribute("src", "img/slow.gif");
+        };
 
+        const checkMatch = () => {
+            properties.clearIntervalStamp = setTimeout(function () {
+                if (properties.computerInput === properties.userInput) {
+                    properties.score += 1;
+                    document.getElementById(properties.computerInput).setAttribute("src", "img/hit.gif");
+                }
+                else {
+                    properties.lives -= 1;
+                    document.getElementById(properties.computerInput).setAttribute("src", "img/miss.gif");
+                };
+            }, properties.resetSpeed);
+        };
 
-        //GAME OVER
-
-        if (lives === 0) {
-            
-            setTimeout(function () {
+        const gameOver = () => {
+            if (properties.lives === 0) {
+                (function () { startBtn.disabled = false; }())
                 for (i = 1; i < 10; i++) {
                     document.getElementById(i).setAttribute("src", `img/L${i}.jpg`);
                 };
-               
-            }, 500);
-
-            finito();
-        };
-
-        function finito() {
-            (function () { button.disabled = false; }())
-            holes.setAttribute("src", "img/default.gif");
-            lives = 3;
-            score = 0;
-            level = 1;
-            mainSpeed = 3000;
-            defaultSpeed = 2500;
-            hitSpeed = 2200;
-            button.setAttribute("src", "img/restart.png");
-            clearInterval(generator);
-            clearInterval(hit);  };
-
-
-        // YOU HAVE WON
-        if (score === 30) {
-            clearInterval(generator);
-            setTimeout(function () {
-                for (i = 1; i < 10; i++) {
-                    document.getElementById(i).setAttribute("src", `img/W${i}.jpg`);
-                };
-            }, 2600);
-        };
-
-
-
-        // LEVEL UP
-        if (score >= 10 && score < 20) {
-            level = 2;
-            mainSpeed = 1800;
-            defaultSpeed = 1300;
-            hitSpeed = 1000;
-            holes.setAttribute("src", "img/medium.gif");
-        }
-        else if (score >= 20) {
-            level = 3;
-            mainSpeed = 1375;
-            defaultSpeed = 875;
-            hitSpeed = 575;
-            holes.setAttribute("src", "img/fast.gif");
-        };
-
-        //CHECK MATCHES
-
-
-        let hit = setTimeout(function () {
-            if (num === user) {
-                score += 1;
-                holes.setAttribute("src", "img/hit.gif");
+                properties.lives = 3;
+                properties.score = 0;
+                properties.level = 1;
+                properties.gameSpeed = 3000;
+                properties.resetSpeed = 2200;
+                startBtn.setAttribute("src", "img/restart.png");
+                clearInterval(generator);
+                clearInterval(properties.clearIntervalStamp);
             }
-            else {
-                lives -= 1;
-                holes.setAttribute("src", "img/miss.gif");
+        };
+
+        const levelUp = () => {
+            if (properties.score >= 10 && properties.score < 20) {
+                properties.level = 2;
+                properties.gameSpeed = 1800;
+                properties.resetSpeed = 1000;
+                document.getElementById(properties.computerInput).setAttribute("src", "img/medium.gif");
+            }
+            else if (properties.score >= 20) {
+                properties.level = 3;
+                properties.gameSpeed = 1375;
+                properties.resetSpeed = 575;
+                document.getElementById(properties.computerInput).setAttribute("src", "img/fast.gif");
             };
-        }, hitSpeed);
+        };
 
-        //RESETS VALUE AFTER EACH CLICK
-        setTimeout(function () {
+        const won = () => {
+            if (properties.score === 30) {
+                clearInterval(generator);
+                setTimeout(function () {
+                    for (i = 1; i < 10; i++) {
+                        document.getElementById(i).setAttribute("src", `img/W${i}.jpg`);
+                    };
+                }, 2600);
+            };
+        }
 
-            user = 0;
-            holes.setAttribute("src", "img/default.gif")
-        }, defaultSpeed)
+        const generator = setInterval(function () {
+            displayState();
+            resetGrid();
+            listenToUserInput();
+            generateComputerInput();
+            checkMatch();
+            levelUp();
+            gameOver();
+            won();
+        }, properties.gameSpeed);
 
 
-    }, mainSpeed);
-
-
-};
+    };
 
 
 
-let button = document.getElementById("hearts");
-button.addEventListener("click", barrel);
-button.addEventListener("click", (function () { button.disabled = true; }));
+    let startBtn = document.getElementById("hearts");
+    startBtn.addEventListener("click", theGame);
+    startBtn.addEventListener("click", (function () { startBtn.disabled = true; }));
 });
